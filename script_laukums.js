d08ed69679a3dea2a1c640e0c6adcb678a5b37b0
rubr <3k1aire@gmail.com>
2021-05-14 00:38:34 +0300

    laukums done? I think??

--- script_laukums.js
+++ script_laukums.js
@@ -24,8 +24,21 @@ function clear() {
     }
 }
 
+var len = 0;
+var time = 0;
+var timer;
 function start() {
-  var x, text;
+    statu = [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0];
+    click = 0;
+    click1 = -1;
+    click2 = -1;
+    time = 0;
+    
+    clearInterval(timer);
+    timer = setInterval(function() { time++; document.getElementById("timetext").innerHTML = "Time: " + String(time) + " seconds"; }, 1000);
+    document.getElementById("timetext").innerHTML = "Time: 0 seconds";
+    
+    var x, text;
 
   // Get the value of the input field with id="size"
   x = document.getElementById("size").value;
@@ -60,9 +73,11 @@ function start() {
       default:
             text = "uhhhh....";
   }
-  document.getElementById("disp").innerHTML = text;
+  //document.getElementById("disp").innerHTML = text;
   
-  sajukums(parseInt(x*x/2));
+  len = parseInt(x*x/2);
+  
+  sajukums();
 }
 
 let list1 = ["possum","axolotl","olm","lyrebird","stick bug","sun bear","capybara","parrotfish","okapi","red-lipped batfish",
@@ -72,7 +87,7 @@ let list2 = ["possum.png","axolotl.png","olm.png","lyrebird.png","stickbug.png",
     "narwhal.png"];
 let saraksts=[];
 let type=[];
-function sajukums(len){
+function sajukums(){
     saraksts=[];
     type=[]; // 0 = text; 1 = image
     for(let i=0;i<len;i++){
@@ -91,7 +106,7 @@ function sajukums(len){
         type[rand]=type[i];
         type[i]=tmp;
     }
-    document.getElementById("disp").innerHTML = saraksts;
+    //document.getElementById("disp").innerHTML = saraksts;
     
     var xxx = document.getElementsByClassName("hover-thingy");
     for (let i = 0; i < xxx.length; i++) {
@@ -107,6 +122,7 @@ function sajukums(len){
         {
             var x = document.getElementById(String(i+1)).getElementsByTagName('button');
             x[0].style.display = "inline";
+            x[0].style.visibility = "hidden";
             var xx = x[0].getElementsByTagName('span');
             xx[0].innerHTML = saraksts[i];
         }
@@ -114,8 +130,110 @@ function sajukums(len){
         {
             var x = document.getElementById(String(i+1)).getElementsByTagName('img');
             x[0].style.display = "inline";
+            x[0].style.visibility = "hidden";
             x[0].src = "images/" + saraksts[i];
+            
+            var xxx = document.getElementById(String(i+1)).getElementsByTagName('button');
+            var xx = xxx[0].getElementsByTagName('span');
+            xx[0].innerHTML = saraksts[i]; // this is here for comparison puposes, it doesn't get displayed
         }
     }
 }
 
+var click = 0;
+var click1 = -1;
+var click2 = -1;
+var statu = [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0];
+
+function change(idee) {
+    var actid = parseInt(idee.id) - 1;
+    if (statu[actid] == 0)
+    {
+        statu[actid] = 1;
+        click++;
+        
+        switch (click)
+        {
+            case 1:
+                click1 = actid;
+                if (type[actid] == 0)
+                {
+                    var x = idee.getElementsByTagName('button');
+                    x[0].style.visibility = "visible";
+                }
+                else
+                {
+                    var x = idee.getElementsByTagName('img');
+                    x[0].style.visibility = "visible";
+                }
+                break;
+                
+            case 2:
+                click2 = actid;
+                if (type[actid] == 0)
+                {
+                    var x = idee.getElementsByTagName('button');
+                    x[0].style.visibility = "visible";
+                }
+                else
+                {
+                    var x = idee.getElementsByTagName('img');
+                    x[0].style.visibility = "visible";
+                }
+                if (list1.indexOf(saraksts[click1]) == list2.indexOf(saraksts[click2]) && list1.indexOf(saraksts[click1]) != -1 && list2.indexOf(saraksts[click2]) != -1 ||
+                list2.indexOf(saraksts[click1]) == list1.indexOf(saraksts[click2]) && list2.indexOf(saraksts[click1]) != -1 && list1.indexOf(saraksts[click2]) != -1)
+                {
+                    statu[click1] = 2;
+                    statu[click2] = 2;
+                    click = 0;
+                    click1 = -1;
+                    click2 = -1;
+                    let win_question = true;
+                    for (let i = 0; i < len; i++)
+                    {
+                        if (statu[i] != 2) win_question = false;
+                    }
+                    if (win_question == true) win();
+                }
+                break;
+                
+            case 3:
+                statu[click1] = 0;
+                statu[click2] = 0;
+                statu[actid] = 0;
+                if (type[click1] == 0) {
+                    var x = document.getElementById(String(click1+1)).getElementsByTagName('button');
+                    x[0].style.visibility = "hidden";
+                }
+                else {
+                    var x = document.getElementById(String(click1+1)).getElementsByTagName('img');
+                    x[0].style.visibility = "hidden";
+                }
+                if (type[click2] == 0) {
+                    var x = document.getElementById(String(click2+1)).getElementsByTagName('button');
+                    x[0].style.visibility = "hidden";
+                }
+                else {
+                    var x = document.getElementById(String(click2+1)).getElementsByTagName('img');
+                    x[0].style.visibility = "hidden";
+                }
+                click = 0;
+                click1 = -1;
+                click2 = -1;
+                break;
+        }
+        //document.getElementById("disp").innerHTML = String(click);
+        //document.getElementById("disp2").innerHTML = String(statu);
+        //document.getElementById("disp3").innerHTML = String(list1.indexOf(saraksts[click1])) + "/" + 
+                //String(list2.indexOf(saraksts[click2]));
+        
+        
+    }
+}
+
+function win() {
+    clearInterval(timer);
+    document.getElementById("timetext").innerHTML = "You win! Final time: " + String(time) + " seconds";
+}
+
+clear();
